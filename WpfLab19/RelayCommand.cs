e307efb95d79dfc96f3002a68025abc204ee1f1b
HinaChleck <hinachleck@gmail.com>
2022-03-14 22:15:57 +0500

    Добавьте файлы проекта.

---
+++ WpfLab19/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfLab19
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;//Таким образом передали управление
+            remove=>CommandManager.RequerySuggested -= value;//событием CanExecuteChanged КоммандМэнеджеру. Эта часть будет общая для все команд. Отличаться они будут только CanExecute и Execute
+        }
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute)//создаем конструктор
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;    
+        }
+        public bool CanExecute(object parameter)=>canExecute?.Invoke(parameter) ?? true;
+        
+
+        public void Execute(object parameter)=>execute(parameter);
+        
+    }
+}
---
+++ WpfLab19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfLab19.Models;
+
+namespace WpfLab19.ViewModels
+{
+    class MainWindowViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private int radius;
+        public int Radius
+        { 
+            get { return radius; }
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            } 
+        }
+        private double length;
+        public double Length
+        {
+            get { return length; }
+            set
+            {
+                length = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public ICommand LengthCommand { get; }
+        private void OnLengthCommandExecute(object p)
+        {
+            Length = Ariph.Length(Radius);
+        }
+        private bool CanLengthCommandExecuted(object p)
+        {
+            if (Radius != 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            LengthCommand = new RelayCommand(OnLengthCommandExecute, CanLengthCommandExecuted);
+        }
+    }
+}
